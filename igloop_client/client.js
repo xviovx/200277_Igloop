c0b69e42b0dd6f8cfbf7bf08daba7e54ce45bb82
xviovx <71275388+xviovx@users.noreply.github.com>
2021-06-09 10:36:49 +0200

    Chats page responsive

--- igloop_client/client.js
+++ igloop_client/client.js
@@ -3,13 +3,10 @@ var querystring = require("querystring");
 var fs = require("fs");
 var templates = require("es6-template-strings");
 
-//setup client socket
 var io = require("socket.io-client");
 
-//connect the socket to the server
 var socket = io("http://localhost:8888", {transports: ["websocket"]});
 
-//GET request response
 var handleFormGet = function(request, response){
     response.writeHead(200, {"Content-Type": "text/html"});
 
@@ -22,35 +19,60 @@ var handleFormGet = function(request, response){
     });
 };
 
-//POST request response
 var handleFormPost = function(request, response){
     response.writeHead(200, {"Content-Type" : "text/html"});
 
 
     var payload = "";
 
-    //event on data received
     request.on("data", function(data) {
         payload += data;
     });
 
-    //event on the end of request
     request.on("end", function(){
         response.writeHead(200, {"Content-Type" : "text/html"});
 
         var post = querystring.parse(payload);
 
-        //request login on the server
         socket.emit("login:data", post);
 
-        fs.readFile("templates/chats.html", "utf8", function(err, data) {
-            if(err) {throw err;};
-
-            var compiled = templates(data, {username: post['username']});
-
-            response.write(compiled);
-            response.end();
-        });
+        if(post['username'] === "Kyle" && post['password'] === "abc123"){
+            fs.readFile("templates/chats.html", "utf8", function(err, data) {
+                if(err) {throw err;};
+    
+                var compiled = templates(data, {username: post['username']});
+    
+                response.write(compiled);
+                response.end();
+            });
+        }else if(post['username'] === "Armand" && post['password'] === "easypassword123"){
+            fs.readFile("templates/chats.html", "utf8", function(err, data) {
+                if(err) {throw err;};
+    
+                var compiled = templates(data, {username: post['username']});
+    
+                response.write(compiled);
+                response.end();
+            });
+        }else if(post['username'] === "Dexter" && post['password'] === "whenitrainsitpours"){
+            fs.readFile("templates/chats.html", "utf8", function(err, data) {
+                if(err) {throw err;};
+    
+                var compiled = templates(data, {username: post['username']});
+    
+                response.write(compiled);
+                response.end();
+            });
+        }else{
+            fs.readFile("templates/error.html", "utf8", function(err, data) {
+                if(err) {throw err;};
+    
+                var compiled = templates(data, {username: post['username']});
+    
+                response.write(compiled);
+                response.end();
+            });
+        }
     });
 }
 
@@ -60,16 +82,13 @@ var simpleRouter = function(request) {
     var method = request.method;
     var path = request.url;
 
-    //handle the query
     var queryIndex = path.indexOf("?");
     if (queryIndex >= 0){
         path = request.url.slice(0, queryIndex);
     }
 
-    //route input by user
     var providedRoute = {method: method, path: path};
 
-    //available routes to navigate the user to 
     var routes = [
         {method: "GET", path: "/", handler: handleFormGet},
         {method: "POST", path: "/chats", handler: handleFormPost}
@@ -85,7 +104,6 @@ var simpleRouter = function(request) {
     return null;
 }
 
-//on request
 server.on("request", function(request, response){
     var handler = simpleRouter(request);
 
@@ -97,7 +115,7 @@ server.on("request", function(request, response){
     }
 });
 
-//client side to listen on port 3000
+
 server.listen(3000, function(){
     console.log("Listening on port 3000");
 });
